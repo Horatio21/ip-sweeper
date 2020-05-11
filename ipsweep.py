a382ac09d1a2d5e49f4c889a88d41eda42f42c2a
Horatiu Oancea <oanceahoratiu@gmail.com>
2020-05-11 09:35:20 +0300

    Added basic ip sweeping functionalities to the script

---
+++ ipsweep.py
@@ -0,0 +1,32 @@
+import argparse
+import ipaddress
+import pythonping
+import ifaddr
+
+
+def sweep_network(args):
+    ip_interface = ipaddress.IPv4Interface(args.ip + "/" + args.netmask)
+    for ip in ip_interface.network:
+        r = str(pythonping.ping(str(ip), count=1, timeout=1))
+        if "Reply" in r:
+            tokens = r.split(" ")
+            print(tokens[2].strip(","))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="This script sweeps ips, by pinging everything in a given network"
+    )
+    parser.add_argument(
+        "ip", nargs="?",
+        help="a network ip, or a host ip (default: this device's network will be swept)",
+        default=ifaddr.get_adapters()[0].ips[-1].ip
+    )
+    parser.add_argument(
+        "netmask", nargs="?",
+        help="the netmask for the ip provided (default: 255.255.255.0)",
+        default="255.255.255.0"
+    )
+
+    args = parser.parse_args()
+    sweep_network(args)
